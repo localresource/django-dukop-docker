1c1d7130c33701574a886f47af3a69aa4cb936e8
localresource <localresource@protonmail.ch>
2019-05-26 19:52:05 +0200

    run.sh: Detach

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sudo docker inspect my-django-dukop >/dev/null
 if [[ $? -eq 0 ]]; then
 	sudo docker start my-django-dukop
 else
-	sudo docker run --tty --user "1000:1000" --volume "$SCRIPT_DIR/django-dukop:/myapp" --name my-django-dukop django-dukop
+	sudo docker run --tty --user "1000:1000" --volume "$SCRIPT_DIR/django-dukop:/myapp" --name my-django-dukop -d django-dukop
 fi
 
 ADDR=$( sudo docker inspect -f '{{range .NetworkSettings.Networks}} http://{{.IPAddress}}:8000{{end}}' my-django-dukop )
